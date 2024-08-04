d86354f91875928cab220f8cb995f95fcff198cb
Bosire Nyakundi <Nyakundi@hotmail.co.uk>
2024-08-04 06:13:45 +0100

    Looks like it's getting  the right answer, i've not double checked on the hevy app yet

--- hevy/v1/client.py
+++ hevy/v1/client.py
@@ -2,41 +2,49 @@ import requests
 
 import aiohttp
 import asyncio
-from datetime import datetime, date
+from datetime import datetime, date, timedelta
 import hevy.conf as conf
-from hevy.v1.schemas.custom import PaginatedWorkouts
+from hevy.v1.schemas.custom import PaginatedWorkouts, PaginatedExerciseTemplates
 from hevy.v1.schemas.base import Workout, ExerciseTemplate
 from typing import AsyncGenerator, Generator
 from collections import Counter
+from async_lru import alru_cache
+from time import time
+from datetime import timedelta
 
 WORKOUT_PREFIX = "/v1/workouts"
 EXERCISE_TEMPLATE_PREFIX = "/v1/exercise_templates"
+
 
 class HevyClientV1:
     def __init__(self, root_url: str, api_key: str):
         self.root_url = root_url
         self.api_key = api_key
-        self.headers = headers = {
-            "api-key": self.api_key
-        }
+        self.headers = headers = {"api-key": self.api_key}
 
     async def _get(self, session, url: str) -> dict:
         async with session.get(url, headers=self.headers) as response:
             return await response.json()
-    
+
     async def _get_page(self, session, url: str, page: int = 1) -> dict:
         url = f"{url}?page={page}"
         return await self._get(session, url)
 
-    async def list_workouts_after_date(self, target_date: date) -> AsyncGenerator[Workout, None]:
+    async def list_workouts_after_date(
+        self, target_date: date
+    ) -> AsyncGenerator[Workout, None]:
         page = 1
         workouts_prefix = WORKOUT_PREFIX
         workouts_endpoint = f"{self.root_url}{workouts_prefix}"
         async with aiohttp.ClientSession() as session:
             while True:
+
+                start_w_time = time()
                 data = await self._get_page(
                     session=session, url=workouts_endpoint, page=page
                 )
+                print(f"Time taken to get page: {time() - start_w_time}")
+
                 paginated_workout = PaginatedWorkouts.model_validate(data)
 
                 for workout in paginated_workout.workouts:
@@ -49,7 +57,29 @@ class HevyClientV1:
                 # Update URL to next page
                 page += 1
 
-    async def get_exercise_type(self, id:str) -> ExerciseTemplate:
+    async def list_exercise_templates(
+        self, page: int = 1
+    ) -> AsyncGenerator[ExerciseTemplate, None]:
+        exercise_template_prefix = EXERCISE_TEMPLATE_PREFIX
+        exercise_template_endpoint = f"{self.root_url}{exercise_template_prefix}"
+        page = 1
+        last_page = None
+        async with aiohttp.ClientSession() as session:
+            while True:
+                if last_page and page > last_page:
+                    return
+                data = await self._get_page(
+                    session=session, url=exercise_template_endpoint, page=page
+                )
+                exercise_templates = PaginatedExerciseTemplates.model_validate(data)
+                if not last_page:
+                    last_page = exercise_templates.page_count
+                for exercise_template in exercise_templates.exercise_templates:
+                    yield exercise_template
+                page += 1
+
+    @alru_cache(maxsize=128)
+    async def get_exercise_template(self, id: str) -> ExerciseTemplate:
         exercise_template_prefix = EXERCISE_TEMPLATE_PREFIX
         exercise_template_endpoint = f"{self.root_url}{exercise_template_prefix}/{id}"
         async with aiohttp.ClientSession() as session:
@@ -58,30 +88,42 @@ class HevyClientV1:
             return exercise_template
 
 
-
 async def main():
     client = HevyClientV1(
         root_url=conf.get_hevy_api_root_url(), api_key=conf.get_hevy_api_key()
     )
 
-    target_date = date(2024, 7, 29)
+    exercise_templates = {}
+
+    target_date = datetime.today() - timedelta(days=7)
+    target_date = target_date.date()
     body_part_counter = Counter()
     async for workout in client.list_workouts_after_date(target_date):
         for exercise in workout.exercises:
             exercise_template_id = exercise.exercise_template_id
-            exercise_template = await client.get_exercise_type(exercise_template_id)
 
+            if exercise_template_id not in exercise_templates:
+                exercise_template = await client.get_exercise_template(
+                    exercise_template_id
+                )
+                exercise_templates[exercise_template_id] = exercise_template
+            else:
+                exercise_template = exercise_templates[exercise_template_id]
+
+            # Get all non warm up sets and ad them to the count for a body part
+            num_sets = len([s for s in exercise.sets if s.set_type != "warmup"])
 
             primary_body_part = exercise_template.primary_muscle_group
-            body_part_counter[primary_body_part] += 1
-
+            body_part_counter[primary_body_part] += num_sets
+            # Do the same for the secondary muscle groups
             for body_part in exercise_template.secondary_muscle_groups:
-                body_part_counter[body_part] += 1 # weight volume on secondary muscle group arbitrarily less
+                body_part_counter[
+                    body_part
+                ] += (
+                    num_sets  # weight volume on secondary muscle group arbitrarily less
+                )
 
-    
     print(body_part_counter)
-
-
 
 
 if __name__ == "__main__":
